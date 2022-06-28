4a01f13fe3c649db6a627f936e5fe4e5b4302842
Samy Doreau <samy.doreau@railsbank.com>
2022-06-28 09:50:54 +0100

    Custom singular test

---
+++ dbtlearn/tests/consistent_created_at.sql
@@ -0,0 +1,12 @@
+-- Checks that there is no review date that is submitted before its listing was created
+-- Make sure that every review date in fct_reviews is more recent than the associated created_at in dim_listings_cleansed
+
+with a as (
+    select * from {{ ref('fct_reviews') }} fr 
+    left join {{ ref('dim_listings_cleansed') }} dl
+    on fr.listing_id = dl.listing_id
+    where fr.created_at > dl.created_at
+)
+
+select * from a
+
